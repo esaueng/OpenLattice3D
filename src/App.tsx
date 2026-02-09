a9dbb3fe013466ec667d79e66ec54e8e623e2934
Peter <171875562+petergustafson@users.noreply.github.com>
2026-02-09 02:08:55 -0500

    Add notification test button

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,26 @@ import { FeedbackWidget } from './components/FeedbackWidget';
 import './App.css';
 
 function App() {
+  const handleTestNotification = async () => {
+    if (!('Notification' in window)) {
+      window.alert('Notifications are not supported in this browser.');
+      return;
+    }
+
+    const permission = Notification.permission === 'default'
+      ? await Notification.requestPermission()
+      : Notification.permission;
+
+    if (permission !== 'granted') {
+      window.alert('Notification permission was not granted.');
+      return;
+    }
+
+    new Notification('Notification test', {
+      body: 'If you can read this, notifications are working.',
+    });
+  };
+
   return (
     <div className="app">
       <LeftPanel />
@@ -18,6 +38,9 @@ function App() {
           Built by Esau Engineering
         </a>
       </div>
+      <button className="btn btn-small notification-test-btn" onClick={handleTestNotification}>
+        Test notification
+      </button>
     </div>
   );
 }
